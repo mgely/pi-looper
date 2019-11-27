73def646c1290d7c2637a724b9e8b5d4d4de2f7f
Mario Gely - TNW <mfgely@tudelft.net>
2019-11-27 21:33:23 +0100

    ideas

--- src/core_new.py
+++ src/core_new.py
@@ -1,6 +1,11 @@
 #####################
-# NEXT: use python scheduler for example to call self.one() at each start of a bar
-# which can also trigger some states
+# NEXT: 
+#
+# Use python scheduler for example to call self.one() at each start of a bar
+# which can also trigger for example the start of rec. 
+# The end of rec should be dealt with internally with a scheduled self.end_of_rec() event
+# 
+#  
 #####################
 
 
@@ -19,37 +24,55 @@ GPIO_leds = [14,18,24,8]
 GPIO_buttons = [15,23,25,7]
 
 class Looper(object):
+
     states = ['rec', 'play']
 
-    def __init__(self):
+    transitions = [
+        # trigger                       # source        # destination
+        ['release_rec_button',          'play',         'rec'],
+        ['release_play_button',         'rec',          'play'],
+    ]
 
+    def __init__(self):
+        self.machine = Machine(model = self,states = self.states, transitions = self.transitions, 
+            initial = 'initial')
+        self.init_hardware()
+
+    def init_hardware(self):
+
+        # LEDs
         self.rec_led = LED(18)
         self.play_led = LED(8)
+        self.back_led = LED(14)
+        self.forw_led = LED(24)
 
-        self.machine = Machine(model = self,states = self.states,initial='rec')
-        self.machine.add_transition('press_rec', 'play', 'rec')
-        self.machine.add_transition('press_play', 'rec', 'play')
+        # Buttons
+        self.rec_button = Button(23)
+        self.play_button = Button(7)
+        self.back_button = Button(15)
+        self.forw_button = Button(25)
+
+        # Button events
+        self.rec_button.when_deactivated = self.release_rec_button
+        self.play_button.when_deactivated = self.release_play_button
     
+    def all_leds_off(self):
+        for l in [self.rec_led,self.play_led]:
+            l.off()
+
+    def on_enter(self):
+        self.all_leds_off()
+
     def on_enter_play(self):
+        self.on_enter()
         self.play_led.on()
-        self.rec_led.off()
         
     def on_enter_rec(self):
-        self.play_led.off()
+        self.on_enter()
         self.rec_led.on()
-        self.machine.add_transition('bar_started', 'rec', 'play')
 
 
 if __name__ == "__main__":
-    l = Looper()
-    rec_button = Button(23)
-    play_button = Button(7)
-
-
-    rec_button.when_deactivated = l.press_rec
-    play_button.when_deactivated = l.press_play
-
-    
-    # play_button.
+    l = Looper()    
     while True:
         sleep(1)
